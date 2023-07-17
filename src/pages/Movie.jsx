99b6aefd1f7f5b51b31ceeab0a38f3b83f590789
Matheus Gomes <matheusoutros@hotmail.com>
2023-07-17 20:31:33 -0300

    finalizando o projeto

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -22,12 +22,67 @@ const Movie = () => {
         const res = await fetch(url)
         const data = await res.json();
 
+        console.log(data)
         setMovie(data);
     }
 
+    useEffect(() => {
+        const filmesURL = ` ${moviesURL}${id}?${apiKey} `
+
+        getMovie(filmesURL);
+    }, []);
+
+
+    const formatCurrency = (number) => {
+        return number.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+        });
+    }
 
     return (
-        <div>Movie</div>
+        <div className="movie-page">
+            {/* {movie && movie.title} */}
+
+            {movie && (
+                <>
+                    <MovieCard Filmes={movie} showLink={false} />
+                    <p className="tagline">{movie.tagline}</p>
+                    <div className="info">
+                        <h3>
+                            <BsWallet2 /> Orçamento
+                        </h3>
+                        <p>
+                            {formatCurrency(movie.budget)}
+                        </p>
+                    </div>
+                    <div className="info">
+                        <h3>
+                            <BsGraphUp /> Receita:
+                        </h3>
+                        <p>
+                            {formatCurrency(movie.revenue)}
+                        </p>
+                    </div>
+                    <div className="info">
+                        <h3>
+                            <BsHourglassSplit /> Minutos:
+                        </h3>
+                        <p>
+                            {movie.runtime}
+                        </p>
+                    </div>
+                    <div className="info description">
+                        <h3>
+                            <BsFillFileEarmarkTextFill /> Descrição:
+                        </h3>
+                        <p>
+                            {movie.overview}
+                        </p>
+                    </div>
+                </>
+            )}
+        </div>
     )
 }
 export default Movie;
